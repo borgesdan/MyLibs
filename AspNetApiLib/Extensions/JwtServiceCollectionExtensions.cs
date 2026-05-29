171adac93332ede499c7355f3c6a6a6446b73cb7
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-29 14:54:37 -0300

    Implementa JwtServiceCollectionExtensions

---
+++ AspNetApiLib/Extensions/JwtServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MyAspNetApiLib.Extensions
+{
+    public static class JwtServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adiciona uma configuração para uso de Jwt bearer token. Os dados são obtidos do appsettings.json através do IConfiguration.
+        /// </summary>
+        public static IServiceCollection AddJwtConfiguration(this IServiceCollection services, JwtServiceConfiguration configuration)
+        {
+            var key = !string.IsNullOrWhiteSpace(configuration.Key) ? configuration.Key : throw new InvalidOperationException("O parâmetro Key está nulo ou vazio.");
+            var issue = !string.IsNullOrWhiteSpace(configuration.Issue) ? configuration.Issue : throw new InvalidOperationException("O parâmetro Issue está nulo ou vazio.");
+            var audience = !string.IsNullOrWhiteSpace(configuration.Audience) ? configuration.Audience : throw new InvalidOperationException("O parâmetro Audience está nulo ou vazio.");
+
+            var simetricKey = Encoding.ASCII.GetBytes(key);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(simetricKey),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issue,
+                    ValidAudience = audience,
+                    ClockSkew = TimeSpan.Zero,
+                };
+
+                if(configuration.Events != null)
+                    options.Events = configuration.Events;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ AspNetApiLib/Extensions/JwtServiceConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+
+namespace MyAspNetApiLib.Extensions
+{    
+    public class JwtServiceConfiguration
+    {
+        public string Key { get; set; } = null!;
+        public string Issue { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public JwtBearerEvents? Events { get; set; }
+
+        public static JwtServiceConfiguration FromConfiguration(IConfiguration configuration)
+        {
+            var key = configuration["Jwt:Key"]
+                ?? throw new NullReferenceException("Chave Jwt:Key não encontrada nas configurações.");
+
+            var issue = configuration["Jwt:Issuer"]
+                ?? throw new NullReferenceException("Chave Jwt:Issuer não encontrada nas configurações.");
+
+            var audience = configuration["Jwt:Audience"]
+                ?? throw new NullReferenceException("Chave Jwt:Audience não encontrada nas configurações.");
+
+            return new JwtServiceConfiguration
+            {
+                Key = key,
+                Issue = issue,
+                Audience = audience
+            };
+        }
+
+        public JwtServiceConfiguration WithRequestCookiesEvent(string cookieKey)
+        {
+            Events ??= new JwtBearerEvents();
+
+            Events.OnMessageReceived = context =>
+            {
+                // Se o cookie existir, extrai o JWT dele e entrega para o validador do .NET
+                if (context.Request.Cookies.ContainsKey(cookieKey))
+                {
+                    context.Token = context.Request.Cookies[cookieKey];
+                }
+
+                return Task.CompletedTask;
+            };            
+
+            return this;
+        }
+    }
+}
