e0a82e1be0fe336aff5b03da64e3a05f05940244
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-29 12:31:48 -0300

    Serviço JWT

---
+++ AspNetApiLib/Security/IJwtService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace MyAspNetApiLib.Security
+{
+    /// <summary>
+    /// Representa o retorno do serviço de geração de token JWT.
+    /// </summary>
+    /// <param name="Token">O token JWT gerado.</param>
+    public record JwtServiceResult(string Token);
+
+    public record JwtServiceRequest(string Key, string Issuer, string Audience, int Expires);
+
+    public interface IJwtService
+    {
+        JwtServiceResult GenerateJwtToken(List<Claim> claims, JwtServiceRequest request);
+    }
+}
---
+++ AspNetApiLib/Security/JwtService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyAspNetApiLib.Security
+{
+    public class JwtService : IJwtService
+    {
+        public JwtServiceResult GenerateJwtToken(List<Claim> claims, JwtServiceRequest request)
+        {
+            ThrowIfInvalidRequest(request);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(request.Key);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(request.Expires),
+                Issuer = request.Issuer,
+                Audience = request.Audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var result = tokenHandler.WriteToken(token);
+
+            return new JwtServiceResult(result);
+        }
+
+        private static void ThrowIfInvalidRequest(JwtServiceRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Key)
+                || string.IsNullOrWhiteSpace(request.Issuer)
+                || string.IsNullOrWhiteSpace(request.Audience))
+            {
+                throw new InvalidOperationException("A requisição para criação de um token Jwt contém dados nulos ou vazios.");
+            }
+        }
+    }
+}
