e2779c4f8a0bb628aa0318a3a89c4d6bb1d66347
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-29 14:28:40 -0300

    Implementa GlobalExceptionHandler

---
+++ AspNetApiLib/Exceptions/HttpStatusCodeException.cs
@@ -0,0 +1,20 @@
+﻿namespace MyAspNetApiLib.Exceptions
+{
+    /// <summary>
+    /// Representa uma exceção com um status code específico.
+    /// </summary>
+    public class HttpStatusCodeException : Exception
+    {
+        public HttpStatusCodeException(int code, string message) : base(message) 
+        {
+            StatusCode = code;
+        }
+
+        public HttpStatusCodeException(int code, string message, Exception? innerException) : base(message, innerException) 
+        {
+            StatusCode = code;
+        }
+
+        public int StatusCode { get; protected set; }
+    }
+}
---
+++ AspNetApiLib/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyAspNetApiLib.Exceptions;
+using System.Net;
+
+namespace MyAspNetApiLib.Middlewares
+{
+    /// <summary>
+    /// Middleware para tratamento de exceções.
+    /// </summary>
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            ProblemDetails? problemDetails;
+            
+            if(exception is HttpStatusCodeException scEx)
+                problemDetails = HandleStatusCodeException(scEx.StatusCode, scEx);
+            else if (exception is BadHttpRequestException bad && bad.StatusCode != StatusCodes.Status500InternalServerError)
+                problemDetails = HandleStatusCodeException(bad.StatusCode, bad);            
+            else
+                problemDetails = HandleInternalError(exception);
+
+            httpContext.Response.StatusCode = problemDetails.Status!.Value;
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            // Retorna 'true' para avisar o ASP.NET que a exceção já foi tratada
+            // e ele não precisa tentar tratá-ela de outra forma ou derrubar o app.
+            return true;
+        }
+
+        private ProblemDetails HandleStatusCodeException(int statusCode, Exception ex)
+        {
+            _logger.LogWarning("Status code {}: '{message}'.", statusCode, ex.Message);
+
+            return new ProblemDetails
+            {
+                Status = statusCode,
+                Title = "Operação inválida",
+                Detail = ex.Message,
+            };
+        }        
+
+        private ProblemDetails HandleInternalError(Exception ex)
+        {
+            _logger.LogError(ex, "Status code 500: Foi capturada uma exceção não tratada.");
+
+            //Monta uma resposta padronizada usando o formato ProblemDetails (Padrão RFC 7807)
+            return new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Erro Interno do Servidor",
+                Detail = "Ocorreu um erro interno.",
+            };
+        }
+    }
+}
